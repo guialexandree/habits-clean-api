7b4a4597492dd4bea6b394c998a0ce3763a4239b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-25 18:48:55 -0300

    chore: adiciona configurações do jest

--- jest.config.js
+++ jest.config.js
@@ -4,12 +4,16 @@ module.exports = {
 		'<rootDir>/src/**/*.ts',
 		'!<rootDir>/src/main/**',
 		'!<rootDir>/src/**/*protocols.ts',
-		'!<rootDir>/src/**/*protocols/index.ts'
+		'!<rootDir>/src/**/*protocols/index.ts',
+		'!<rootDir>/src/**/index.ts'
 	],
 	coverageDirectory: 'coverage',
 	preset: '@shelf/jest-mongodb',
 	watchPathIgnorePatterns: ['globalConfig'],
 	transform: {
 		'.+\\.ts$': 'ts-jest'
+	},
+	moduleNameMapper: {
+		'@/(.*)': '<rootDir>/src/$1'
 	}
 }
