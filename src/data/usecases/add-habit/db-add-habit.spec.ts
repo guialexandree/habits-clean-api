f1002deee29ea64df73641c80ac38814f9fa1ea9
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-25 19:04:44 -0300

    test: deve progragar erro caso repositório lançe exceção

--- src/data/usecases/add-habit/db-add-habit.spec.ts
+++ src/data/usecases/add-habit/db-add-habit.spec.ts
@@ -1,4 +1,5 @@
 import { DbAddHabit } from './db-add-habit'
+import { throwError } from '@/domain/test'
 import { AddHabitReposistorySpy } from '@/data/test'
 
 type SutTypes = {
@@ -27,5 +28,22 @@ describe('Caso de uso - Adicionar Hábito', () => {
 
 		await sut.add(addHabitParams)
 		expect(dbAddReposistorySpy.addHabitParams).toEqual(addHabitParams)
+	})
+
+	test('Deve propagar o erro se dbAddHabitRepository lançar exceção', async () => {
+		const { sut, dbAddReposistorySpy } = makeSut()
+
+		jest
+			.spyOn(dbAddReposistorySpy, 'add')
+			.mockImplementationOnce(throwError)
+
+		const addHabitParams = {
+			title: 'txt01',
+			createdAt: new Date().toISOString()
+		}
+
+		const promise = sut.add(addHabitParams)
+
+		await expect(promise).rejects.toThrow()
 	})
 })
