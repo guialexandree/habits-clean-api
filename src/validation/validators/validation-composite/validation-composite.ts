8df8615e04c4245c7e93caaf207b98794059c0a6
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-26 20:37:41 -0300

    feat: adiciona ValidationComposite

---
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -0,0 +1,16 @@
+import { Validation } from '@/presentation/protocols'
+
+export class ValidationComposite implements Validation {
+  constructor (private readonly validations: Validation[]) { }
+
+  validate (input: any): Error {
+    for (const validation of this.validations) {
+      const erro = validation.validate(input)
+      if (erro) {
+        return erro
+      }
+    }
+
+    return null
+  }
+}
