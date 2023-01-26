f5f176154fae7679568dad8e05000368d90a1527
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-25 22:29:36 -0300

    feat: deve retornar status 400 se Validation falhar

--- src/presentation/controllers/add-habit/add-habit-controller.ts
+++ src/presentation/controllers/add-habit/add-habit-controller.ts
@@ -1,12 +1,20 @@
+import { badRequest, serverError } from '@/presentation/helpers'
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 
 export class AddHabitController implements Controller {
 	constructor (private readonly validation: Validation) {}
 
 	async handle (request: AddHabitController.Request): Promise<HttpResponse> {
-		this.validation.validate(request)
+		try {
+			const error = this.validation.validate(request)
+			if (error) {
+				return badRequest(error)
+			}
 
-		return { body: null, statusCode: 300 }
+			return null
+		} catch (error) {
+			return serverError(error)
+		}
 	}
 }
 
---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,22 @@
+import { InternalServerError } from '@/presentation/errors'
+import { HttpResponse } from '@/presentation/protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error
+})
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new InternalServerError(error.stack)
+})
+
+export const noContent = (): HttpResponse => ({
+  statusCode: 204,
+  body: null
+})
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data
+})
