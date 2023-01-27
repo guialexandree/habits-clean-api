f282eec12266ab7797802acab17e3fc6184cfd2d
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-27 17:57:48 -0300

    fix: remove arquivos não utilizados

--- src/main/routes/habit-routes.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify'
-import { makeHabitController } from '@/main/factories/controllers'
-import { adaptRoute } from '@/main/adapters'
-
-export const habitRoutes = (router: FastifyInstance): void => {
-  router.post('/habits', adaptRoute(makeHabitController()))
-}
