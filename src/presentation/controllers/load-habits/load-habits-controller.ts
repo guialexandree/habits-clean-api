ddf1586722167ab070610487a50734cc6ed072e1
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-28 12:48:32 -0300

    feat: adiciona tiapgem de data para o parametro date

--- src/presentation/controllers/load-habits/load-habits-controller.ts
+++ src/presentation/controllers/load-habits/load-habits-controller.ts
@@ -1,9 +1,11 @@
 import { Controller, HttpResponse, Validation } from '@/presentation/protocols'
 import { badRequest, serverError } from '@/presentation/helpers'
+import { LoadHabits } from '@/domain/usecases'
 
 export class LoadHabitsController implements Controller {
 	constructor (
-		private readonly validation: Validation
+		private readonly validation: Validation,
+		private readonly dbLoadHabits: LoadHabits
 	) {}
 
 	async handle (request: LoadHabitsController.Request): Promise<HttpResponse> {
@@ -12,6 +14,10 @@ export class LoadHabitsController implements Controller {
 			if (error) {
 				return badRequest(error)
 			}
+
+			const { date } = request
+			const parsedDate = new Date(date)
+			await this.dbLoadHabits.load(parsedDate)
 		} catch (error) {
 			return serverError(error)
 		}
