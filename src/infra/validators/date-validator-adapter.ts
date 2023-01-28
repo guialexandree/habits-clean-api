71120441cdd408ab91e1ee0b46cbc3e00ae9f2ee
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-28 15:22:52 -0300

    feat: loadHabits deve retornar status 200 e lista de hábitos com sucesso

--- src/infra/validators/date-validator-adapter.ts
+++ src/infra/validators/date-validator-adapter.ts
@@ -2,7 +2,7 @@ import { DateValidator } from '@/validation/protocols'
 
 export class DateValidatorAdapter implements DateValidator {
 	isValid (date: string): boolean {
-		const regex = /^\d{4}[\\-](0?[1-9]|1[012])[\\-](0?[1-9]|[12][0-9]|3[01])$/
-    return regex.test(date)
+		const regex = /^\d{4}[\\-](0[1-9]{1}|1[012]{1}){2}[\\-](0?[1-9]|[12][0-9]|3[01])$/
+		return regex.test(date)
   }
 }
