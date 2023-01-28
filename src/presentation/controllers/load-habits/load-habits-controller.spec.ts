a4fedcb212bf453e01464844c53d49e730e58a7b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-28 11:38:00 -0300

    test: loadHabitsController deve retornar status 500 se Validation lançar exceção

--- src/presentation/controllers/load-habits/load-habits-controller.spec.ts
+++ src/presentation/controllers/load-habits/load-habits-controller.spec.ts
@@ -1,8 +1,9 @@
 import { LoadHabitsController } from './load-habits-controller'
 import { ValidationSpy } from '@/presentation/test'
-import { badRequest } from '@/presentation/helpers'
+import { badRequest, serverError } from '@/presentation/helpers'
 import MockDate from 'mockdate'
 import faker from 'faker'
+import { throwError } from '@/domain/test'
 
 const mockRequest = (): LoadHabitsController.Request => ({
 	date: faker.date.recent().toISOString()
@@ -48,5 +49,16 @@ describe('AddHabit Controller', () => {
     const httpResponse = await sut.handle(mockRequest())
 
     expect(httpResponse).toEqual(badRequest(validationSpy.error))
+  })
+
+	test('Deve retornar status 500 se Validation lançar exceção', async () => {
+    const { sut, validationSpy } = makeSut()
+    jest
+      .spyOn(validationSpy, 'validate')
+      .mockImplementationOnce(throwError)
+
+    const response = await sut.handle(mockRequest())
+
+    expect(response).toEqual(serverError(new Error()))
   })
 })
