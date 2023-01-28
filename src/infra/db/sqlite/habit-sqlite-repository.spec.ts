5a4cbf60c0e6f4ffbce1893da6d236af27ba57fd
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-28 10:29:18 -0300

    test: loadHabitsRepository deve retornar array vazio caso não encontre habítos possíveis

--- src/infra/db/sqlite/habit-sqlite-repository.spec.ts
+++ src/infra/db/sqlite/habit-sqlite-repository.spec.ts
@@ -3,10 +3,29 @@ import { HabitSqliteRepository } from './habit-sqlite-repository'
 import { AddHabit } from '@/domain/usecases'
 import faker from 'faker'
 import MockDate from 'mockdate'
+import { WeekDay } from '@/main/types'
+
+const createNewHabit = async (createdAt = faker.date.recent(), weekDay: number): Promise<void> => {
+	await prismaClient.habit.create({
+		data: {
+			title: faker.random.words(4),
+			created_at: createdAt,
+			weekDays: {
+				create: {
+					week_day: weekDay
+				}
+			}
+		}
+	})
+}
 
 const makeAddHabitParams = (): AddHabit.Params => ({
 	title: faker.datatype.uuid(),
-	weekDays: [0, 2, 5],
+	weekDays: [
+		WeekDay.SATURDAY,
+		WeekDay.MONDAY,
+		WeekDay.THURSDAY
+	],
 	createdAt: new Date()
 })
 
@@ -33,14 +52,45 @@ describe('Habit Sqlite Repository', () => {
 		MockDate.reset()
 	})
 
-	test('Deve adicionar o hábito com sucesso', async () => {
+	describe('add()', () => {
+		test('Deve adicionar o hábito com sucesso', async () => {
+			const { sut } = makeSut()
+			const addHabitParams = makeAddHabitParams()
+
+			const { id } = await sut.add(addHabitParams)
+
+			const count = await prismaClient.habit.count()
+			expect(count).toBe(1)
+			expect(id).toBeTruthy()
+		})
+	})
+
+	describe('loadByDateAndWeekDay', () => {
+		test('Deve retornar lista com habítos possíveis na data', async () => {
+			const { sut } = makeSut()
+			const searchDate = new Date('2023-01-22')
+			const weekDay = searchDate.getDay()
+
+			await createNewHabit(new Date('2023-01-21'), weekDay === 6 ? (weekDay - 1) : (weekDay + 1))
+			await createNewHabit(new Date('2023-01-21'), weekDay)
+			await createNewHabit(new Date('2023-01-22'), weekDay)
+			await createNewHabit(new Date('2023-01-23'), weekDay)
+
+			const list = await sut.loadByDateAndWeekDay(searchDate, weekDay)
+			expect(list.length).toBe(2)
+		})
+	})
+
+	test('Deve retornar array vazio caso não encontre habítos possíveis', async () => {
 		const { sut } = makeSut()
-		const addHabitParams = makeAddHabitParams()
+		const searchDate = new Date('2023-01-19')
+		const weekDay = searchDate.getDay()
 
-		const { id } = await sut.add(addHabitParams)
+		await createNewHabit(new Date('2023-01-22'), weekDay)
+		await createNewHabit(new Date('2023-01-23'), weekDay)
 
-		const count = await prismaClient.habit.count()
-		expect(count).toBe(1)
-		expect(id).toBeTruthy()
+		const list = await sut.loadByDateAndWeekDay(searchDate, weekDay)
+		expect(list.length).toBe(0)
+		expect(list).toEqual([])
 	})
 })
