7773067b40b4f221c941c82605333ca9df4fe91a
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-28 11:34:49 -0300

    test: loadHabitsController deve retornar status 400 se Validation falhar

--- src/presentation/controllers/load-habits/load-habits-controller.spec.ts
+++ src/presentation/controllers/load-habits/load-habits-controller.spec.ts
@@ -1,7 +1,12 @@
 import { LoadHabitsController } from './load-habits-controller'
 import { ValidationSpy } from '@/presentation/test'
+import { badRequest } from '@/presentation/helpers'
 import MockDate from 'mockdate'
 import faker from 'faker'
+
+const mockRequest = (): LoadHabitsController.Request => ({
+	date: faker.date.recent().toISOString()
+})
 
 type SutTypes = {
 	sut: LoadHabitsController
@@ -29,12 +34,19 @@ describe('AddHabit Controller', () => {
 
 	test('Deve chamar Validation com os valores corretos', async () => {
     const { sut, validationSpy } = makeSut()
-    const request = {
-			date: faker.date.recent().toISOString()
-		}
+    const request = mockRequest()
 
     await sut.handle(request)
 
     expect(validationSpy.input).toEqual(request)
+  })
+
+	test('Deve retornar status 400 se Validation falhar', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new Error()
+
+    const httpResponse = await sut.handle(mockRequest())
+
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
