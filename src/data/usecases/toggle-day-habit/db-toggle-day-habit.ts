8112bb62b85aad4e3e2ce699ff34be6b1fc5c77b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-29 14:01:29 -0300

    feat: dbToggleDayHabit deve chamar removeDayHabit com o dayId correto

--- src/data/usecases/toggle-day-habit/db-toggle-day-habit.ts
+++ src/data/usecases/toggle-day-habit/db-toggle-day-habit.ts
@@ -1,15 +1,21 @@
 import { ToggleDayHabit } from '@/domain/usecases/'
-import { LoadCompletedHabitsRepository, LoadDayRepository } from '@/data/protocols'
+import {
+	LoadCompletedHabitsRepository,
+	LoadDayRepository,
+	RemoveDayHabitRepository
+} from '@/data/protocols'
 
 export class DbToggleDayHabit implements ToggleDayHabit {
 	constructor (
 		private readonly loadDayRepository: LoadDayRepository,
-		private readonly loadCompletedHabitsRepository: LoadCompletedHabitsRepository
+		private readonly loadCompletedHabitsRepository: LoadCompletedHabitsRepository,
+		private readonly removeDayHabitRepository: RemoveDayHabitRepository
 	) {}
 
 	async toggle (habitId: string): Promise<void> {
 		const today = new Date()
-		await this.loadDayRepository.loadOrCreate(today)
+		const dayId = await this.loadDayRepository.loadOrCreate(today)
 		await this.loadCompletedHabitsRepository.loadByDate(today)
+		await this.removeDayHabitRepository.removeById(dayId)
 	}
 }
