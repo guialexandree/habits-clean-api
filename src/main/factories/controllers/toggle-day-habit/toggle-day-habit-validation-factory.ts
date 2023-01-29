6cd3fe35798a4e9ca6ba0c90e9cf34554942e853
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-29 17:32:14 -0300

    feat: garante que toggleDayHabitController vai chamar o validation com as validações corretas

---
+++ src/main/factories/controllers/toggle-day-habit/toggle-day-habit-validation-factory.ts
@@ -0,0 +1,12 @@
+import { Validation } from '@/presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+
+export const makeToggleDayHabitValidation = (): ValidationComposite => {
+	const validations: Validation[] = []
+
+	for (const field of ['id']) {
+		validations.push(new RequiredFieldValidation(field))
+	}
+
+	return new ValidationComposite(validations)
+}
