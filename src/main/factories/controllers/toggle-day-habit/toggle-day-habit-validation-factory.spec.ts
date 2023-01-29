ceec57e397ac311e8a562b07e6422497ca8de407
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-29 17:32:44 -0300

    test: garante que toggleDayHabitController vai chamar o validation com as validações corretas

---
+++ src/main/factories/controllers/toggle-day-habit/toggle-day-habit-validation-factory.spec.ts
@@ -0,0 +1,17 @@
+import { Validation } from '@/presentation/protocols'
+import { RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+import { makeToggleDayHabitValidation } from '@/main/factories/controllers'
+
+jest.mock('@/validation/validators/validation-composite/validation-composite')
+
+describe('LoadHabitsValidation Factory', () => {
+  test('Deve chamar ValidationComposite com as validações corretas', () => {
+    makeToggleDayHabitValidation()
+    const validations: Validation[] = []
+
+    for (const field of ['id']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+    expect(ValidationComposite).toHaveBeenCalledWith(validations)
+  })
+})
