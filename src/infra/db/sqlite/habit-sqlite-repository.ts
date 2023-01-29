a71f953e470284128bdb5498232f82e35fd31be2
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-29 02:13:46 -0300

    feat: adiciona implementação de addDayHabit repository

--- src/infra/db/sqlite/habit-sqlite-repository.ts
+++ src/infra/db/sqlite/habit-sqlite-repository.ts
@@ -1,10 +1,18 @@
 import { AddHabit } from '@/domain/usecases'
-import { AddHabitRepository, LoadCompletedHabitsRepository, LoadPossibleHabitsRepository, LoadDayHabitRepository, LoadDayRepository, RemoveDayHabitRepository } from '@/data/protocols'
+import {
+	AddHabitRepository,
+	LoadCompletedHabitsRepository,
+	LoadPossibleHabitsRepository,
+	LoadDayHabitRepository,
+	LoadDayRepository,
+	RemoveDayHabitRepository,
+	AddDayHabitRepository
+} from '@/data/protocols'
 import { prismaClient } from './prisma-client'
 import { SqliteHelper } from './sqlite-helper'
 
-export class HabitSqliteRepository implements AddHabitRepository, LoadPossibleHabitsRepository, LoadCompletedHabitsRepository, LoadDayRepository, LoadDayHabitRepository, RemoveDayHabitRepository {
-	async add (data: AddHabit.Params): Promise<AddHabitRepository.Result> {
+export class HabitSqliteRepository implements AddHabitRepository, LoadPossibleHabitsRepository, LoadCompletedHabitsRepository, LoadDayRepository, LoadDayHabitRepository, RemoveDayHabitRepository, AddDayHabitRepository {
+	async addHabit (data: AddHabit.Params): Promise<AddHabitRepository.Result> {
 		const { title, createdAt, weekDays } = data
 
 		const { id } = await prismaClient.habit.create({
@@ -94,6 +102,15 @@ export class HabitSqliteRepository implements AddHabitRepository, LoadPossibleHa
 		await prismaClient.dayHabit.delete({
 			where: {
 				id: dayHabitId
+			}
+		})
+	}
+
+	async addDayHabit (habitId: string, dayId: string): Promise<void> {
+		await prismaClient.dayHabit.create({
+			data: {
+				day_id: dayId,
+				habit_id: habitId
 			}
 		})
 	}
