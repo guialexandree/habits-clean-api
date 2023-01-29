ebf8d58ae2cb72116478eaf480b0246a1228a892
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-29 13:16:42 -0300

    feat: dbToggleDayHabit deve chamar chamar loadCompletedHabits com a data correta

--- src/data/usecases/toggle-day-habit/db-toggle-day-habit.ts
+++ src/data/usecases/toggle-day-habit/db-toggle-day-habit.ts
@@ -1,11 +1,15 @@
 import { ToggleDayHabit } from '@/domain/usecases/'
-import { LoadDayRepository } from '@/data/protocols'
+import { LoadCompletedHabitsRepository, LoadDayRepository } from '@/data/protocols'
 
 export class DbToggleDayHabit implements ToggleDayHabit {
-	constructor (private readonly loadDayRepository: LoadDayRepository) {}
+	constructor (
+		private readonly loadDayRepository: LoadDayRepository,
+		private readonly loadCompletedHabitsRepository: LoadCompletedHabitsRepository
+	) {}
 
 	async toggle (habitId: string): Promise<void> {
 		const today = new Date()
 		await this.loadDayRepository.loadOrCreate(today)
+		await this.loadCompletedHabitsRepository.loadByDate(today)
 	}
 }
