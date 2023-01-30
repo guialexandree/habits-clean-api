9acaffd3d662f0fb6c6861740d14bcfa4fa5d692
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-30 19:39:57 -0300

    feat: adiciona nova rota toggleDayHabit

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { adaptRoute } from '@/main/adapters'
-import { makeAddHabitController, makeLoadHabitsController } from '@/main/factories'
+import {
+	makeAddHabitController,
+	makeLoadHabitsController,
+	makeToggleDayHabitController
+} from '@/main/factories'
 
 export default (app: FastifyInstance): void => {
 	app.post('/habits', adaptRoute(makeAddHabitController()))
 	app.get('/day', adaptRoute(makeLoadHabitsController()))
+	app.patch('/habits/:habitId/toggle', adaptRoute(makeToggleDayHabitController()))
 }
