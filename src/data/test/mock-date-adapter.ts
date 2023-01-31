336905deb18a0eb85c89ff421030c72c325f1d3b
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-31 20:08:54 -0300

    test: adiciona adapter para tratar datas

---
+++ src/data/test/mock-date-adapter.ts
@@ -0,0 +1,30 @@
+import { DateValidator } from '@/validation/protocols'
+import { DateStartOf, DateStartToday } from '@/data/protocols'
+
+export class DateValidatorAdapterSpy implements DateValidator {
+	date: string
+	result = true
+
+	isValid (date: string): boolean {
+		this.date = date
+		return this.result
+	}
+}
+
+export class DateStartTodayAdapterSpy implements DateStartToday {
+	result = new Date()
+
+	startOfToday (): Date {
+		return this.result
+	}
+}
+
+export class DateStartOfAdapterSpy implements DateStartOf {
+	date: string
+	result = new Date()
+
+	startOf (date: string): Date {
+		this.date = date
+		return this.result
+	}
+}
