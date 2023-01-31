6e328e00440a29c97c54a03942b28ac53e1cf3d7
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-31 20:06:20 -0300

    refactor: removida informações desnecessárias

--- src/main/server.ts
+++ src/main/server.ts
@@ -9,6 +9,6 @@ setupApp()
 				port: Number(process.env.PORT),
 				host: '0.0.0.0'
 			})
-		console.log(`Servidor rodando na porta ${String(process.env.PORT)}`)
+		console.log(`Servidor rodando na porta ${String(process.env.PORT)}\nProcesso: ${process.pid}`)
 	})
 	.catch(error => { console.error(error) })
