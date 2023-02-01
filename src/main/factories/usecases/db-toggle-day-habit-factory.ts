f82bc0961bf9d91703af4f694e3bd986119871a1
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-31 21:37:04 -0300

    feat: adiciona dateAdapter na composição da controller

--- src/main/factories/usecases/db-toggle-day-habit-factory.ts
+++ src/main/factories/usecases/db-toggle-day-habit-factory.ts
@@ -1,13 +1,16 @@
 import { DbToggleDayHabit } from '@/data/usecases'
 import { ToggleDayHabit } from '@/domain/usecases'
+import { DateAdapter } from '@/infra/adapters'
 import { HabitSqliteRepository } from '@/infra/db/sqlite'
 
 export const makeDbToggleDayHabit = (): ToggleDayHabit => {
 	const habitsRepository = new HabitSqliteRepository()
+	const dateAdapter = new DateAdapter()
 	return new DbToggleDayHabit(
 		habitsRepository,
 		habitsRepository,
 		habitsRepository,
-		habitsRepository
-		)
+		habitsRepository,
+		dateAdapter
+	)
 }
