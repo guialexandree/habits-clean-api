906389798f3bbf63b68c0ea3f18b5ed0006af229
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-02-01 22:19:38 -0300

    feat: DbToggleDayHabit deve chamar checkHabitByIdRepository com o habitId correto

--- src/data/usecases/toggle-day-habit/db-toggle-day-habit.ts
+++ src/data/usecases/toggle-day-habit/db-toggle-day-habit.ts
@@ -1,23 +1,25 @@
 import { ToggleDayHabit } from '@/domain/usecases/'
 import {
 	AddDayHabitRepository,
+	CheckHabitByIdRepository,
 	DateStartToday,
 	LoadDayHabitRepository,
 	LoadDayRepository,
 	RemoveDayHabitRepository
 } from '@/data/protocols'
 
-// TODO: criari checkHabitIsValid()
 export class DbToggleDayHabit implements ToggleDayHabit {
 	constructor (
 		private readonly loadDayRepository: LoadDayRepository,
 		private readonly loadDayHabitRepository: LoadDayHabitRepository,
 		private readonly removeDayHabitRepository: RemoveDayHabitRepository,
 		private readonly addDayHabitRepository: AddDayHabitRepository,
+		private readonly checkHabitByIdRepository: CheckHabitByIdRepository,
 		private readonly dateAdapter: DateStartToday
 	) {}
 
 	async toggle (habitId: string): Promise<void> {
+		await this.checkHabitByIdRepository.checkById(habitId)
 		const today = this.dateAdapter.startOfToday()
 		const dayId = await this.loadDayRepository.loadOrCreate(today)
 		const completedHabitId = await this.loadDayHabitRepository.loadCompletedHabit(habitId, dayId)
